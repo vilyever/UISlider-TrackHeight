bbd13448fb4f55e3c29c5e2cc2681a77eb6dcc67
vilyever <250450035@163.com>
2015-08-11 14:20:15 +0800

    Initial Commit

---
+++ UISlider-TrackHeight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISlider-TrackHeight
+//
+//  Created by FTET on 15/8/11.
+//  Copyright (c) 2015年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UISlider-TrackHeight/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UISlider-TrackHeight
+//
+//  Created by FTET on 15/8/11.
+//  Copyright (c) 2015年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
